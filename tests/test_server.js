fbe4cea7360287e858f082c547e2333fb3253cbd
David Bell <thisisdavidbell@hotmail.com>
2016-11-19 23:40:40 +0000

    written tests for POST. Failing as no implementation

--- tests/test_server.js
+++ tests/test_server.js
@@ -1,36 +1,75 @@
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
-
 var Storage = require('./../Storage');
-
 var request = require("supertest");
 
-
-
-// This agent refers to PORT where program is runninng.
-
-// var server = supertest.agent("http://localhost:8080");
-
-
 describe('Testing server', function() {
+  // if server already running outside test
+  //var server = request.agent("http://localhost:8080");
   var server = require('./../server');
+
   describe('/api/todos', function() {
-
-    it("should return status 200",function(done){
-
+   describe('GET - Get all', function() {
+  // order of 'it's matters as
+    it("should return status 200", function(done){
+      // if server already running outside test
+//      server
       request(server)
       .get("/api/todos")
+      .expect(200)
       .end(function(err,res){
-        console.log("res: " + JSON.stringify(res, null, 2));
-        console.log("err: " + err);
-        expect(err).to.equal(null);
-        // HTTP status should be 200
-        expect(res.status).to.equal(200);
-        // Error key should be false.
+//        console.log("res: " + JSON.stringify(res, null, 2));
+//        console.log("err: " + err);
 
+//        expect(err).to.equal(null); // if server is brought up and running, no err
+        expect(res.status).to.equal(200); // HTTP status should be 200
         done();
       });
-
     });
-  });
+
+    it("should be empty to start with", function(done){
+      // if server already running outside test
+//      server
+      request(server)
+      .get("/api/todos")
+      .expect(200)
+      .end(function(err,res){
+        var parsed_body = JSON.parse(res.text);
+  //      console.log(res.text);
+        expect(err).to.equal(null); // if server is brought up and running, no err
+        expect(parsed_body).to.be.an('object');
+        expect(parsed_body).to.be.empty;
+        done();
+      });
+    });
+  }); // end GET
+
+   describe('POST - Create Todo', function() {
+  // order of 'it's matters as
+    it("should return status 201", function(done){
+      request(server)
+      .post("/api/todos")
+      .send('{"name": "thename", "text": "some text", "done": "false"}')
+      .end(function(err,res){
+        console.log(JSON.stringify(res));
+        expect(res.status).to.equal(201);  // HTTP status should be 201 for POST
+        done();
+      }); // end .end
+    }); // end it
+
+    it("should return all objects", function(done){
+      request(server)
+      .post("/api/todos")
+      .send('{"name": "thename", "text": "some text", "done": "false"}')
+      .end(function(err,res){
+        console.log(JSON.stringify(res));
+        expect(res.status).to.equal(201);  // HTTP status should be 201 for POST
+// should this be a new POST? Test inspect the response contains newly created object.
+        done();
+      }); // end .end
+    }); // end it
+
+  }); // end describe POST
+
+ })
 });
